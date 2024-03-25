e939ddc79c827046c84f197d72340d43425225ab
Salvador <salvador.lopez@edgebound.com>
2024-03-25 14:18:19 -0600

    feat: add functionality for gestion to authors

--- src/controllers/author.controller.ts
+++ src/controllers/author.controller.ts
@@ -31,4 +31,107 @@ export default class AuthorController {
             if (this.connection) await this.server.app.locals.dbConnection.release(this.connection)
         }
     }
+
+    async findAuthors(term: string, limit: number, page: number): Promise<IResponse> {
+        try {
+            this.connection = this.server.app.locals.dbConnection
+            
+            if (isNaN(limit) || isNaN(page) || limit <= 0 || page <= 0) {
+                return { ok: false, message: 'Invalid limit or page', response: null, code: 400 };
+            }
+
+            const regex = new RegExp(term ? term : '', 'i')
+
+            const response = await Author.find(
+                {id: regex}
+            )
+            .limit(limit * 1)
+            .skip((page - 1) * limit)
+            .exec()
+
+            if (response.length === 0) {
+                return { ok: false, message: 'No authors found with these terms', response: null, code: 404 };
+            }
+
+            return { ok: true, message: 'Authors found!', response: response, code: 200 }
+        } catch (err: any) {
+            logger.error(`[AuthorController/findAuthors] ${err}`)
+            return { ok: false, message: 'Error ocurred', response: err, code: 500 }
+        } finally {
+            if (this.connection) await this.server.app.locals.dbConnection.release(this.connection)
+        }    
+    }
+
+    async getAuthorById(idAuthor: string): Promise<IResponse> {
+        try{
+            this.connection = this.server.app.locals.dbConnection
+
+            if (!idAuthor) {
+                return { ok: false, message: 'Invalid id', response: null, code: 400 };
+            }
+
+            const response: any = await Author.findOne({_id: idAuthor})
+
+        if (!response) {
+            return { ok: false, message: 'Author not found', response: null, code: 404 };
+        }
+
+            return { ok: true, message: 'Found author!', response: response, code: 200 }
+        } catch (err: any) {
+            logger.error(`[AuthorController/getAuthorById] ${err}`)
+            return { ok: false, message: 'Error ocurred', response: err, code: 500 }
+        } finally {
+            if(this.connection) await this.server.app.locals.dbConnection.release(this.connection)
+        }
+    }
+
+    async updateAuthor(author: IAuthor, idAuthor:string): Promise<IResponse> {
+        try{
+            this.connection = this.server.app.locals.dbConnection
+
+            if (!idAuthor) {
+                return { ok: false, message: 'Invalid id', response: null, code: 400 };
+            }
+
+            const response: any = await Author.findOneAndUpdate(
+                {_id: idAuthor}, 
+                author,
+                { returnDocument: 'after' }
+            )
+
+            if (!response) {
+                return { ok: false, message: 'Author not found', response: null, code: 404 };
+            }
+
+            return { ok: true, message: 'Found author!', response: response, code: 200 }
+        } catch (err: any) {
+            logger.error(`[AuthorController/updateAuthor] ${err}`)
+            return { ok: false, message: 'Error ocurred', response: err, code: 500 }
+        } finally {
+            if(this.connection) await this.server.app.locals.dbConnection.release(this.connection)
+        }
+    }
+
+    async deleteAuthor(idAuthor: string): Promise<IResponse> {
+        try{
+            this.connection = this.server.app.locals.dbConnection
+
+            if (!idAuthor) {
+                return { ok: false, message: 'Invalid id', response: null, code: 400 };
+            }
+
+            const response = await Author.findOneAndDelete({_id: idAuthor})
+
+            if (!response) {
+                return { ok: false, message: 'Author not found', response: null, code: 404 };
+            }
+
+            return { ok: true, message: 'Found author!', response: response, code: 200 }
+        } catch (err: any) {
+            logger.error(`[AuthorController/updateAuthor] ${err}`)
+            return { ok: false, message: 'Error ocurred', response: err, code: 500 }
+        } finally {
+            if(this.connection) await this.server.app.locals.dbConnection.release(this.connection)
+        }   
+    }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,7 @@ import AuthorController from "./controllers/author.controller"
 const routes = Router()
 const authorCtrl =  new AuthorController
 
-routes.post('/author_create', async(req: Request, res: Response) => {
+routes.post('/create_author', async(req: Request, res: Response) => {
     try {
         const author = req.body
 
@@ -12,6 +12,51 @@ routes.post('/author_create', async(req: Request, res: Response) => {
         return res.status(response.code).json(response)
     } catch(err: any) {
         return res.status( err.code ? err.code : 500 ).json(err)
+    }
+})
+
+routes.get('/find_authors', async(req: Request, res: Response) => {
+    try{
+        const {term, page, limit} = req.query
+
+        const response = await authorCtrl.findAuthors(String(term), Number(page), Number(limit))
+        return res.status(response.code).json(response)
+    } catch(err: any) {
+        return res.status( err.code ? err.code : 500).json(err)
+    }
+})
+
+routes.get('/get_author/:idAuthor', async(req: Request, res: Response) => {
+    try{
+        const { idAuthor } = req.params
+
+        const response = await authorCtrl.getAuthorById(String(idAuthor))
+        return res.status(response.code).json(response)
+    } catch(err: any) {
+        return res.status(err.code ? err.code : 500).json(err)
+    }
+})
+
+routes.put('/update_author/:idAuthor', async(req: Request, res:Response) => {
+    try {
+        const { idAuthor } = req.params
+        const body = req.body
+        
+        const response = await authorCtrl.updateAuthor(body, idAuthor)
+        return res.status(response.code).json(response)
+    } catch(err: any) {
+        return res.status(err.code ? err.code : 500).json(err)
+    }
+})
+
+routes.delete('/delete_author/:idAuthor', async(req: Request, res: Response) => {
+    try{
+        const { idAuthor } = req.params
+
+        const response = await authorCtrl.deleteAuthor(idAuthor)
+        return res.status(response.code).json(response)
+    } catch(err: any) {
+        return res.status(err.code ? err.code : 500).json(err)
     }
 })
 
